295ee283d325e42fd8a901e42bc14721af34800a
Jim Bennett <jimbobbennett@mac.com>
2014-09-06 13:24:12 +0700

    Upgraded jimlib, added extension to run on main thread

---
+++ JimLib.Xamarin/Extensions/ObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using JimBobBennett.JimLib.Extensions;
+using Xamarin.Forms;
+
+namespace JimBobBennett.JimLib.Xamarin.Extensions
+{
+    [Pure]
+    public static class ObjectExtensions
+    {
+// ReSharper disable once UnusedParameter.Global
+        public static async Task<T> RunOnMainThreadAsync<T>(this object o, Func<Task<T>> func)
+        {
+            var retVal = default(T);
+
+            await Task.Run(() =>
+            {
+                var done = false;
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    retVal = await func();
+                    done = true;
+                });
+
+                retVal.WaitForAsync(() => done, int.MaxValue);
+            });
+
+            return retVal;
+        }
+    }
+}
